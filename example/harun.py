ac12df9480341851c2d084211cee50fc0920676c
Barkin Kahraman <barbarkin1@gmail.com>
2024-10-27 14:54:19 +0300

    talha
    talha

--- example/harun.py
+++ example/harun.py
@@ -1,13 +1,1 @@
-
-s1 = int(input("1.sayıyı girin:"))
-s2 = int(input("2.sayıyı girin:"))
-s3 = int(input("3.sayıyı girin:"))
-s4 = int(input("4.sayıyı girin:"))
-toplam =s1+s2+s3+s4
-print ('toplama: ',toplam)
-çarpma = s1*s2*s3*s4
-print ('çarpma:',çarpma)
-çıkartma = (s2-s1)-(s3-s4)
-print  ('çıkarma :',çıkartma)
-bölme =(s2/s1)/(s3/s4)
-print ('bölme: ',bölme)
+#burada problem var
