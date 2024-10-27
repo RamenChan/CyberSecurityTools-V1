69bf6cdf8353f7f23c1ae5bbd30f97af0fb0d278
harun <harunemirkale932@gmail.com>
2024-10-27 14:58:24 +0300

    harun.py v2
    harun.py v2

--- example/harun.py
+++ example/harun.py
@@ -4,9 +4,9 @@ s1 = 12
 s2 = 24
 s3 = 54
 s4 = 9
-toplam =s1,s2,s3,s4
-çarpma = s1,s2,s3,s4
-çıkartma = (s2,s1),(s3,s4)
-print = ('toplama: ',toplam)
-print = ( 'çıkarmt :',çıkartma)
-print = ('çarpma:',çarpma)
+toplam =s1+s2,+s3,+s4
+çarpma = s1*s2*s3*s4
+çıkartma = (s2-s1),(s3-s4)
+print ('toplama: ',toplam)
+print  ('çıkarma :',çıkartma)
+print ('çarpma:',çarpma)
