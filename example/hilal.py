30d78e458aec2682ad40a3e385019d9f2516b58d
hilalyoney <hilalyoney@hotmail.com>
2024-10-28 15:47:37 +0300

    dörtislem

---
+++ example/hilal.py
@@ -0,0 +1,17 @@
+sayi1 = 75
+sayi2 = 50
+sayi3 = 25
+sayi4 = 8
+
+toplam = sayi1 + sayi2 + sayi3 + sayi4
+print( 'toplam: ', toplam )
+cikarma = sayi1 - sayi2 - sayi3 - sayi4
+print ('cikarma:', cikarma)
+carpma = sayi1 * sayi2 * sayi3 * sayi4
+print ('carpma:', carpma)
+if sayi1 == 0 or sayi2 == 0 or sayi3 == 0 or sayi4 ==0 :
+    print('tanımsız')
+else:
+    bölme = (sayi1 / sayi2 ) / (sayi3 / sayi4)
+    print ('bölme:', bölme)
+
