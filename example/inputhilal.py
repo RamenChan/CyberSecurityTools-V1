a531d7c6277f8b1ef32e1392cdf06f1140b309a4
hilalyoney <hilalyoney@hotmail.com>
2024-10-28 19:22:29 +0300

    inputdegeralma

---
+++ example/inputhilal.py
@@ -0,0 +1,9 @@
+sayi1, sayi2, sayi3 = map(int, input("Lütfen boşluk ile ayırarak üç sayı giriniz: ").split())
+print(f"Sayılar: {sayi1}, {sayi2}, {sayi3}")
+
+toplam = sayi1 + sayi2 + sayi3
+print(f"Sayıların toplamı: {toplam}")
+carpim = sayi1 * sayi2 * sayi3
+print(f"Sayıların çarpımı: {carpim}")
+cikarma = sayi1 - sayi2 - sayi3
+print(f"Sayıların çıkarımı: {cikarma}")
