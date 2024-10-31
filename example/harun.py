cdd8a38e20b4e96aecf9c9111a07fa7276730a5a
harun <harunemirkale932@gmail.com>
2024-10-31 15:35:20 +0300

    değer alma

--- example/harun.py
+++ example/harun.py
@@ -1,14 +1,13 @@
-#4sayının toplamı ve çarpımı yapımı :
 
-s1 = 12
-s2 = 24
-s3 = 54
-s4 = 9
+s1 = int(input("1.sayıyı girin:"))
+s2 = int(input("2.sayıyı girin:"))
+s3 = int(input("3.sayıyı girin:"))
+s4 = int(input("4.sayıyı girin:"))
 toplam =s1+s2+s3+s4
-çarpma = s1*s2*s3*s4
-çıkartma = (s2-s1)-(s3-s4)
-bölme =(s2/s1)/(s3/s4)
 print ('toplama: ',toplam)
-print  ('çıkarma :',çıkartma)
+çarpma = s1*s2*s3*s4
 print ('çarpma:',çarpma)
+çıkartma = (s2-s1)-(s3-s4)
+print  ('çıkarma :',çıkartma)
+bölme =(s2/s1)/(s3/s4)
 print ('bölme: ',bölme)
